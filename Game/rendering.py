39598dd5666ff17139acc485e75b1933e82f29df
Stefan Hallberg <stefanditlev@gmail.com>
2024-07-02 01:38:11 +0200

    Game can run now, still using high CPU.

--- Game/rendering.py
+++ Game/rendering.py
@@ -1,20 +1,20 @@
+import threading
 import pygame
 import queue
 from Game.graphics import GraphicsHandler
-from Integration.utilities import pygame_lock  # Import the lock from utilities
+from Integration.utilities import pygame_lock
 
 class GameRenderer:
     def __init__(self, screen_width, screen_height, num_agents):
-        pygame.init()
-        pygame.font.init()
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         pygame.display.set_caption("ClimbSmart Multi-Agent")
         self.clock = pygame.time.Clock()
         self.queues = [queue.Queue() for _ in range(num_agents)]
+        self.stop_event = threading.Event()
 
     def render(self):
         running = True
-        while running:
+        while running and not self.stop_event.is_set():
             with pygame_lock:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -26,13 +26,16 @@ class GameRenderer:
                         self._render_frame(render_data)
 
                 pygame.display.flip()
-                self.clock.tick(60)
+                self.clock.tick(60)  # Cap the frame rate to 60 FPS
 
-        pygame.quit()  # Ensure pygame quits properly
+        pygame.quit()
 
     def _render_frame(self, render_data):
-        self.screen.fill((0, 0, 0))  # Clear screen to black before rendering
+        self.screen.fill((0, 0, 0))
         GraphicsHandler.render(self.screen, render_data)
 
     def get_queues(self):
         return self.queues
+
+    def stop(self):
+        self.stop_event.set()
