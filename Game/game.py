adf8a0401aba469196b39d9f80f080d3b51bdfca
Stefan Hallberg <stefanditlev@gmail.com>
2024-07-11 17:19:49 +0200

    minor debugs.

--- Game/game.py
+++ Game/game.py
@@ -17,13 +17,13 @@ sys.path.append(parent_dir)
 from Game.rendering import GameRenderer
 from Integration.training_game import TrainingGame
 
-def monitor_resources(stop_event):
-    process = psutil.Process(os.getpid())
-    while not stop_event.is_set():
-        cpu_usage = process.cpu_percent(interval=1)
-        memory_info = process.memory_info()
-        print(f"CPU Usage: {cpu_usage}%, Memory Usage: {memory_info.rss / (1024 * 1024)} MB")
-        time.sleep(1)
+# def monitor_resources(stop_event):
+#     process = psutil.Process(os.getpid())
+#     while not stop_event.is_set():
+#         cpu_usage = process.cpu_percent(interval=1)
+#         memory_info = process.memory_info()
+#         print(f"CPU Usage: {cpu_usage}%, Memory Usage: {memory_info.rss / (1024 * 1024)} MB")
+#         time.sleep(1)
 
 def run_game_instance_thread(queue, num_agents, screen_width, screen_height, stop_event, verbose=False):
     try:
@@ -34,23 +34,25 @@ def run_game_instance_thread(queue, num_agents, screen_width, screen_height, sto
     except Exception as e:
         print(f"Exception in game instance: {e}")
     finally:
+        game.cleanup()
         print("Game instance terminated")
+
 
 def main():
     pygame.init()
     screen_width, screen_height = 800, 900
     num_agents = 2
 
-    stop_event = threading.Event()
-
     renderer = GameRenderer(screen_width, screen_height, num_agents)
     queues = renderer.get_queues()
+
+    stop_event = threading.Event()
 
     game_thread = threading.Thread(target=run_game_instance_thread, args=(queues, num_agents, screen_width, screen_height, stop_event, False))
     game_thread.start()
 
-    resource_monitor_thread = threading.Thread(target=monitor_resources, args=(stop_event,))
-    resource_monitor_thread.start()
+    # resource_monitor_thread = threading.Thread(target=monitor_resources, args=(stop_event,))
+    # resource_monitor_thread.start()
 
     try:
         renderer.render()
@@ -59,26 +61,25 @@ def main():
     finally:
         print("Terminating game process...")
         stop_event.set()
-        renderer.stop()
         game_thread.join()
-        resource_monitor_thread.join()
+        # resource_monitor_thread.join()
         pygame.quit()
         print("Pygame quit in main")
 
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
+    # profiler = cProfile.Profile()
+    # profiler.enable()
 
     try:
         main()
     except Exception as e:
         print(f"Exception in main: {e}")
-    finally:
-        profiler.disable()
-        s = StringIO()
-        sortby = 'cumulative'
-        ps = pstats.Stats(profiler, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        with open('profile_stats.txt', 'w') as f:
-            f.write(s.getvalue())
-        print("Profiling results saved to profile_stats.txt")
+    # finally:
+    #     profiler.disable()
+    #     s = StringIO()
+    #     sortby = 'cumulative'
+    #     ps = pstats.Stats(profiler, stream=s).sort_stats(sortby)
+    #     ps.print_stats()
+    #     with open('profile_stats.txt', 'w') as f:
+    #         f.write(s.getvalue())
+    #     print("Profiling results saved to profile_stats.txt")
